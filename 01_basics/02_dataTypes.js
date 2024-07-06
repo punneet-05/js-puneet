4f52da3ecb356f4ba52dcddcbb3f536e998d3c10
punneet-05 <puneetjaiswal8036@gmail.com>
2024-07-07 01:20:21 +0530

    Add files via upload

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+let name="puneet"  //string
+let age=20    //no. without doublequotes
+let isLoggedIn = false  //boolean
+
+
+
+
+
+//they all are primitive datatypes
+// number => 2 to power 53(storage)
+// bigint (use for big data like stock market etc)
+// string => ""
+//boolean
+//null=> standalone value
+//undefined
+//symbol=> unique
+
+//0bject
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);     //object
