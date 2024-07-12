a1144aa1c9a5c2e45a28399ebc5e414efca6ea1b
punneet-05 <puneetjaiswal8036@gmail.com>
2024-07-13 01:04:47 +0530

    Add files via upload

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score = "puneet"
+
+console.log(typeof score);
+console.log(typeof(score));   //both are correct way to find the type 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//coversion
+//"33" => 33
+//true=> 1; false =>0
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
