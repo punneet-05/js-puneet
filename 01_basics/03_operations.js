8c4e8501819cbed4900cba4e6c4e3eabb0dc9e2b
punneet-05 <puneetjaiswal8036@gmail.com>
2024-07-17 03:28:08 +0530

    Add files via upload

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1="hello"
+let str2=" puneet"
+
+let str3 = str1 + str2
+console.log(str3);  //hello puneet
+
+console.log("1" + 2);   //12
+console.log(1 + "2");    //12
+console.log("1" + 2 + 3);   //123  //java rules ECMAscripts
+console.log(1 + 2 + "3");  //33
+
+let gameChanger = 100
+++gameChanger;
+console.log(gameChanger);  //101
+
+console.log("7" * "4");  //28
+
+
+
