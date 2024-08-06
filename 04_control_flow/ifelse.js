05aa4600df629ca72a5d651d711f64d433ce2dfa
punneet-05 <puneetjaiswal8036@gmail.com>
2024-08-07 03:05:03 +0530

    Add files via upload

---
+++ 04_control_flow/ifelse.js
@@ -0,0 +1,39 @@
+const isUserloggedIn = true
+const temprature = 41
+
+if ( temprature === 40){
+    console.log("less than 50");
+
+}
+else{
+    console.log("temprature is greater than 50");
+}
+
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInfrmGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==3){
+    console.log("allow to buy course");
+}
+
+if (loggedInfrmGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+}
+
+
+
+
+
+
+
+
+
+
+
+
